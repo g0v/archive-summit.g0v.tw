708aa366d9522d7d187040facf075e39fa0c7f2f
shelling <navyblueshellingford@gmail.com>
2016-01-02 18:50:04 +0800

    Add Footer component

--- app/javascripts/application.js
+++ app/javascripts/application.js
@@ -6,6 +6,7 @@ import "../images/keynote/heusser.jpg";
 import React from "react";
 import ReactDOM from "react-dom";
 import Intro from "./components/intro";
+import Footer from "./components/footer";
 import locale, { getLocale, setLocale } from "./locale";
 
 if (!getLocale()) {
@@ -18,4 +19,5 @@ if (!getLocale()) {
 
 document.addEventListener("DOMContentLoaded", (e) => {
   ReactDOM.render(<Intro />, document.querySelector('[data-react-class="Intro"]'))
+  ReactDOM.render(<Footer />, document.querySelector('[data-react-class="Footer"]'));
 });
