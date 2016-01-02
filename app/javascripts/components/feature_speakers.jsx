3c1297a8b9e55090df1353b8675df1182f9f904b
shelling <navyblueshellingford@gmail.com>
2016-01-02 18:16:55 +0800

    translate feature speakers

--- app/javascripts/components/feature_speakers.jsx
+++ app/javascripts/components/feature_speakers.jsx
@@ -2,22 +2,20 @@
 
 import React, { Component } from "react";
 import Speaker from "./speaker";
+import { getLocale } from "../locale";
+
+import speakers from "../../jsons/feature_speakers.json";
+speakers[getLocale()].map((speaker) => { require(`../../${speaker.image}`); });
 
 class FeatureSpeakers extends Component {
   render() {
     return <div className="feature-speakers">
       <h2>Who Spoke at g0v Summit 2014</h2>
       <div>
-        {this.props.speakers.map((speaker) => { return <Speaker key={speaker.name} className="feature-speaker" speaker={speaker} />; })}
+        {speakers[getLocale()].map((speaker) => { return <Speaker key={speaker.name} className="feature-speaker" speaker={speaker} />; })}
       </div>
     </div>;
   }
 };
-
-FeatureSpeakers.defaultProps = {
-};
-
-FeatureSpeakers.defaultProps.speakers = require("../../jsons/feature_speakers.json");
-FeatureSpeakers.defaultProps.speakers.map((speaker) => { require(`../../${speaker.image}`); });
 
 export default FeatureSpeakers;
