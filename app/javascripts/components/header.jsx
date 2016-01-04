468d3c7f030b6b8b03b928cae89dfe6765c09db0
shelling <navyblueshellingford@gmail.com>
2016-01-04 23:23:26 +0800

    Provide Header component

---
+++ app/javascripts/components/header.jsx
@@ -0,0 +1,15 @@
+'use strict';
+
+import React, { Component } from "react";
+
+class Header extends Component {
+  render() {
+    return <header>
+      <h1>啥米零時政府 g0v 2016 summit</h1>
+      <h2>14-15 May, 2016</h2>
+      <h2>Academia Sinica, Taipei</h2>
+    </header>;
+  }
+};
+
+export default Header;
