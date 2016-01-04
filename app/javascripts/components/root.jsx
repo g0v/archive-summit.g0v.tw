39aff53a7baf46b2a31dbe5abe8f6173bbaaeafd
shelling <navyblueshellingford@gmail.com>
2016-01-04 23:20:52 +0800

    rename .feature to header

--- app/javascripts/components/root.jsx
+++ app/javascripts/components/root.jsx
@@ -7,11 +7,11 @@ import Footer from "./footer";
 class Root extends Component {
   render() {
     return <main>
-      <div className="feature">
+      <header>
         <h1>啥米零時政府 g0v 2016 summit</h1>
         <h2>14-15 May, 2016</h2>
         <h2>Academia Sinica, Taipei</h2>
-      </div>
+      </header>
       <Intro />
       <Footer />
     </main>;
