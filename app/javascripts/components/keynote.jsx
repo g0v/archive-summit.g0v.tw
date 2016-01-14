b020f129950e7ec46fed1707b6f0e1abb2385550
shelling <navyblueshellingford@gmail.com>
2016-01-14 21:06:57 +0800

    Translate keynote description

--- app/javascripts/components/keynote.jsx
+++ app/javascripts/components/keynote.jsx
@@ -9,7 +9,7 @@ import speaker from "../../jsons/keynote_speaker.json";
 class Keynote extends Component {
   render() {
     return <div className="keynote">
-      Returning for a second year after 2014, the g0v Summit 2016 will again invite the global civic tech community to share their experiences for collaboration between public servants, technologists, and NGO workers.  We are pleased to have Felipe Heusser, founder and former Director of the Chilean NGO Ciudadano Inteligente and Fellow at the Berkman Center, as our keynote speaker.
+      {speaker[getLocale()].description}
       <Speaker className="keynote-speaker" speaker={speaker[getLocale()]} />
     </div>;
   }
