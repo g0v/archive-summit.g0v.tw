a9d248a2503197e3de8bb2545348d3e3ed131a6a
MrOrz <johnsonliang7@gmail.com>
2016-02-20 00:16:13 +0800

    Add link and fix typo in milestones

--- app/javascripts/components/milestone.jsx
+++ app/javascripts/components/milestone.jsx
@@ -11,14 +11,23 @@ export default class Milestone extends Component {
     return <div className="milestone">
       <h2>{data.header}</h2>
       <section className="milestones">
-        {data.milestones.map(({range, title}) => (
-          <article className="milestone">
+        {data.milestones.map(({range, title, url, label}) => (
+          <article className="milestone" key={title}>
             <div className="milestone-range">
               <span className="milestone-date">{range[0]}</span>
               －
               <span className="milestone-date">{range[1]}</span>
             </div>
-            <div className="milestone-title">{title}</div>
+            <div className="milestone-title">
+              <a href={url}>
+                {title}
+              </a>
+              {label ? (
+                <div>
+                  <span className="milestone-label">{label}</span>
+                </div>
+              ) : ''}
+            </div>
           </article>
         ))}
       </section>
