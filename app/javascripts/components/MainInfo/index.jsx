81a595d4a0b3cb5b2550393c9fcb686f784c396f
Ly Cheng <lyforever62@hotmail.com>
2016-02-24 02:07:05 +0800

    Use rgba opacity instead of moving background-position

--- app/javascripts/components/MainInfo/index.jsx
+++ app/javascripts/components/MainInfo/index.jsx
@@ -5,19 +5,21 @@ class MainInfo extends Component {
   render() {
     return (
       <div className={styles.root}>
-        <div className={styles.year}>2016 summit</div>
-        <img
-          className={styles.logo}
-          src={require('./images/g0v-logo.png')}
-        />
-        <div>
+        <div className={styles.wrapper}>
+          <div className={styles.year}>2016 summit</div>
           <img
-            className={styles.theme}
-            src={require('./images/theme.png')}
+            className={styles.logo}
+            src={require('./images/g0v-logo.png')}
           />
+          <div>
+            <img
+              className={styles.theme}
+              src={require('./images/theme.png')}
+            />
+          </div>
+          <div className={styles.day}>5. 14-15</div>
+          <div className={styles.place}>中央研究院</div>
         </div>
-        <div className={styles.day}>5. 14-15</div>
-        <div className={styles.place}>中央研究院</div>
       </div>
     );
   }
