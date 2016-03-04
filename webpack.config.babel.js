3460f6840f618480f3a08026ce8362c1b05a940b
MrOrz <johnsonliang7@gmail.com>
2016-03-05 01:08:34 +0800

    Add og:image and og:description

--- webpack.config.babel.js
+++ webpack.config.babel.js
@@ -28,6 +28,7 @@ export default {
       {
         test: /\.(png|jpg)$/,
         loader: 'url?limit=50000',
+        exclude: /og\.png$/,
       },
       {
         test: /app\/images\/speakers2014/,
@@ -76,6 +77,7 @@ export default {
     new HtmlWebpackPlugin({
       title: "啥米零時政府 g0v 2016 summit",
       jld: jld,
+      description: "2014 之後又跳過了一整年，g0v summit 2016 再次邀請全球公民技術社群來分享公務員，技術人員，和非政府組織工作人員之間的協作經驗。我們很高興邀請到 Filipe Heusser 擔任今年的專題演講者，他也是 the Chilean NGO Ciudadano Inteligente 的創辦人和前股東，以及 Berkman Center 的成員。",
       filename: "index.html",
       template: "app/templates/index.html"
     }),
