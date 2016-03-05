9e0336f3580f126a122ee3f8835db6f54eafdee6
billy3321 <billy3321@gmail.com>
2016-03-05 10:25:34 +0800

    fix the lightbox type to use content

--- app/javascripts/components/Lightbox/LightBox.jsx
+++ app/javascripts/components/Lightbox/LightBox.jsx
@@ -1,14 +1,17 @@
 'use strict';
 
 import React, { Component } from "react";
+import { getLocale } from "../../locale";
+import lightbox from "./lightbox.json";
 import styles from "./styles.css";
 
-// usage: <LightBox url="" text="" />
+// usage: <LightBox text="" heading="" content="" />
 
 class LightBox extends Component {
   static propTypes = {
-    url: React.PropTypes.string,
-    text: React.PropTypes.string
+    text: React.PropTypes.string,
+    heading: React.PropTypes.string,
+    content: React.PropTypes.string
   };
 
   constructor(props) {
@@ -27,11 +30,13 @@ class LightBox extends Component {
   render() {
     if (this.state.display) {
       return (
-      <div>
-        <button onClick={this.openLightBox.bind(this)}>{this.props.text}</button>
+        <div>
+          <button onClick={this.openLightBox.bind(this)}>{this.props.text}</button>
           <div className={styles.lightboxwrap}>
-            <div className={styles.lightboxcontent}>
-              <iframe className={styles.lightboxiframe} frameborder="0" allowfullscreen="" src={this.props.url}></iframe>
+            <div className={styles.lightboxcontainer}>
+              <div className={styles.lightboxheading}><h3>{this.props.heading}</h3></div>
+              <p>{this.props.content}</p>
+              <button className={styles.lightboxbutton} onClick={this.closeLightBox.bind(this)}>{lightbox[getLocale()].close}</button>
             </div>
             <button className={styles.lightboxclose} onClick={this.closeLightBox.bind(this)}>x</button>
           </div>
