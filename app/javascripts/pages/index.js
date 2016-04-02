0937dbb8ed70341ed0db1fa1685808ebb658888d
YuTin Liu <yuting1987@gmail.com>
2016-04-02 13:34:18 +0800

    feat(live): #68 page init

--- app/javascripts/pages/index.js
+++ app/javascripts/pages/index.js
@@ -2,4 +2,5 @@ export Home from "./Home";
 export Speakers from "./Speakers";
 export Schedules from "./Schedules";
 export Sponsors from "./Sponsors";
+export Live from "./Live";
 export Transport from "./Transport";
