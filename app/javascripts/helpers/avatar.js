2d7b54607367b08d64d842516ff26fd15c19d675
Chia-liang Kao <clkao@clkao.org>
2016-04-20 21:01:19 +0800

    use hires avatars

--- app/javascripts/helpers/avatar.js
+++ app/javascripts/helpers/avatar.js
@@ -4,9 +4,9 @@ export function avatar(speaker) {
   if (speaker.avatar) {
     return speaker.avatar.match(/^\.\//) ? require(`javascripts/components/SpeakerList/${speaker.avatar.replace(/^\.\//, "")}`) : speaker.avatar;
   } else if (speaker.twitter) {
-    return 'https://avatars.io/twitter/' + speaker.twitter.replace(/^@/, '');
+    return 'https://avatars.io/twitter/' + speaker.twitter.replace(/^@/, '') + '/large';
   } else if (speaker.facebook) {
-    return 'https://avatars.io/facebook/' + speaker.facebook;
+    return 'https://avatars.io/facebook/' + speaker.facebook + '/large';
   } else {
     return require('javascripts/components/SpeakerList/default.png');
   }
