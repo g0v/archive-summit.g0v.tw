f230dd2b1474d00b45b076ce421d958dc170d36c
Chia-liang Kao <clkao@clkao.org>
2016-04-21 12:08:04 +0800

    prefer facebook avatar over twitter

--- app/javascripts/helpers/avatar.js
+++ app/javascripts/helpers/avatar.js
@@ -3,10 +3,10 @@
 export function avatar(speaker) {
   if (speaker.avatar) {
     return speaker.avatar.match(/^\.\//) ? require(`javascripts/components/SpeakerList/${speaker.avatar.replace(/^\.\//, "")}`) : speaker.avatar;
-  } else if (speaker.twitter) {
-    return 'https://avatars.io/twitter/' + speaker.twitter.replace(/^@/, '') + '/large';
   } else if (speaker.facebook) {
     return 'https://avatars.io/facebook/' + speaker.facebook + '/large';
+  } else if (speaker.twitter) {
+    return 'https://avatars.io/twitter/' + speaker.twitter.replace(/^@/, '') + '/large';
   } else {
     return require('javascripts/components/SpeakerList/default.png');
   }
