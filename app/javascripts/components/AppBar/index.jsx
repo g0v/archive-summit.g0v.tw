919d273d2f2136debb362b6f4575926ab4dbf9c8
Pomin Wu <pomin5@gmail.com>
2016-04-28 11:54:19 +0800

    Fixed Unconf link placements
    移到首頁 milestones 不放 menu

--- app/javascripts/components/AppBar/index.jsx
+++ app/javascripts/components/AppBar/index.jsx
@@ -136,15 +136,6 @@ const Menu = () => {
         className={styles.item}
         activeClassName={styles.active}>
         <Link
-          to="/2016/unconf"
-        >
-          {info[getLocale()].unconf}
-        </Link>
-      </li>
-      <li
-        className={styles.item}
-        activeClassName={styles.active}>
-        <Link
           to="/2016/sponsors"
         >
           {info[getLocale()].sponsors}
