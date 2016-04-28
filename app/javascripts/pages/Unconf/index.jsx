64cbe2062cb450e0820defa0d1ed7b5bf632f9d8
Pomin Wu <pomin5@gmail.com>
2016-04-28 12:12:03 +0800

    Fixed Unconf page localization and move text files

--- app/javascripts/pages/Unconf/index.jsx
+++ app/javascripts/pages/Unconf/index.jsx
@@ -3,14 +3,13 @@ import { getLocale } from "javascripts/locale";
 import transports from "jsons/transport.json";
 import hotels from "jsons/hotels.json";
 import styles from "./styles.css";
-
-var content = require(`./${getLocale() === null ? 'en-US' : getLocale() }.md`);
+import content from "text/unconf";
 
 class Unconf extends Component {
     render() {
           return (
               <div className={styles.root}>
-                <div dangerouslySetInnerHTML={{__html: content}}>
+                <div dangerouslySetInnerHTML={{__html: content[getLocale()]}}>
                 </div>
               </div>
           );
---
+++ app/text/unconf/index.js
@@ -0,0 +1,4 @@
+export default {
+  "en-US": require("./en-US.md"),
+  "zh-TW": require("./zh-TW.md")
+};
