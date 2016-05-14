751b82bdf6a2a666f594cdb07eb791fb610b99c6
YuTin Liu <yuting1987@gmail.com>
2016-05-14 12:20:10 +0800

    720p

--- app/javascripts/pages/Live/index.jsx
+++ app/javascripts/pages/Live/index.jsx
@@ -43,8 +43,8 @@ class Live extends Component {
             <div className={styles.control}>
               <a className={styles.channel} href="https://g0v.hackpad.com/g0v-summit--3TyxgUCaVIV" target="_blank">{locale[getLocale()].switch}</a>
               <a className={styles.channel} onClick={() => this.onChangeVenue('R0', 'https://www.youtube.com/embed/MRic6Qt8GKQ', 'https://www.youtube.com/user/g0vTW/live')}>YouTube R0</a>
-              <a className={styles.channel} onClick={() => this.onChangeVenue('R1', 'https://www.youtube.com/embed/4MQX-SAQlZc', 'https://www.youtube.com/watch?v=4MQX-SAQlZc')}>YouTube R1</a>
-              <a className={styles.channel} onClick={() => this.onChangeVenue('R2', 'https://www.youtube.com/embed/FhlMyeEhV6o', 'https://www.youtube.com/watch?v=FhlMyeEhV6o')}>YouTube R2</a>
+              <a className={styles.channel} onClick={() => this.onChangeVenue('R1', 'https://www.youtube.com/embed/P4X4L2pRz2g', 'https://www.youtube.com/watch?v=P4X4L2pRz2g')}>YouTube R1</a>
+              <a className={styles.channel} onClick={() => this.onChangeVenue('R2', 'https://www.youtube.com/embed/4vJb_5K4hYk', 'https://www.youtube.com/watch?v=4vJb_5K4hYk')}>YouTube R2</a>
               <a className={styles.channel} onClick={() => this.onChangeVenue('R0', 'https://livehouse.in/embed/channel/g0v/video', 'https://livehouse.in/channel/g0v.tw')}>LIVEhouse.in R0</a>
               <a className={styles.channel} onClick={() => this.onChangeVenue('R1', 'https://livehouse.in/embed/channel/g0v1/video', 'https://livehouse.in/channel/g0v1')}>LIVEhouse.in R1</a>
               <a className={styles.channel} onClick={() => this.onChangeVenue('R2', 'https://livehouse.in/embed/channel/g0v2/video', 'https://livehouse.in/channel/g0v2')}>LIVEhouse.in R2</a>
